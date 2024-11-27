dbd664bef2d9f26f0c9b6b3a06d43f098737f59e
Efim <efim0424@mail.ru>
2024-11-27 13:50:24 +0300

    add email verification

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard page')
+
+@section('content')
+    <h1>Dashboard page</h1>
+@endsection
---
+++ resources/views/user/verify-email.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Login form')
+
+@section('content')
+    <div class="alert alert-info" role="all">
+        Thank you for registering! A link to confirm your registration has been sent to your email.
+    </div>
+    <div>
+        Didn't receive the link?
+        <form method="POST" action="">
+            @csrf
+            <button type="submit" class="btn btn-link ps-0">Send link</button>
+        </form>
+    </div>
+@endsection
