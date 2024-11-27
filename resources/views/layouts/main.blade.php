23c45371437e2df3daf59aab6cf8ea06574eea2f
Efim <efim0424@mail.ru>
2024-11-27 11:14:08 +0300

    feat: add user registration to the database

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Auth')</title>
-    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
 </head>
 
 <body>
@@ -41,8 +41,27 @@
         </div>
     </nav>
 
-    <main class="main mt-3">
+    <main class="main my-3">
         <div class="container">
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            @if (session('success'))
+                <div class="alert alert-success">
+                    <ul>
+                        {{ session('success') }}
+                    </ul>
+                </div>
+            @endif
+
             @yield('content')
         </div>
     </main>
